1f90e5ed5ed074c45fc0cc045ee3caaf2579931d
Oyerinde Samuel <oyerindesamuelabiodun@gmail.com>
2021-09-28 14:04:42 -0700

    ..

--- static-job-listings-master/data.js
+++ static-job-listings-master/data.js
@@ -1,4 +1,4 @@
-[
+const menu = [
   {
     "id": 1,
     "company": "Photosnap",
@@ -150,3 +150,62 @@
     "tools": ["React", "Sass"]
   }
 ]
+
+// get parent element
+const sectionCenter = document.querySelector(".section-center");
+const btnContainer = document.querySelector(".btn-container");
+const filterBtns = document.querySelectorAll(".filter-btn");
+
+// display all items when page loads
+window.addEventListener("DOMContentLoaded", function () {
+  diplayMenuItems(menu);
+  displayMenuButtons();
+});
+
+function diplayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    // console.log(item);
+
+    return `<article class="menu-item">
+          <div class="container">
+            <img src=${item.img} alt=${item.title} class="photo" />
+            <div class="item-info">
+              <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">$${item.price}</h4>
+              </header>
+              <p class="item-text">
+                ${item.desc}
+              </p>
+            </div>
+          <div>
+        </article>`;
+  });
+  displayMenu = displayMenu.join("");
+  // console.log(displayMenu);
+
+  sectionCenter.innerHTML = displayMenu;
+}
+
+
+
+
+
+
+filterBtns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    // console.log(e.currentTarget.dataset);
+    const category = e.currentTarget.dataset.id;
+    const menuCategory = menu.filter(function (menuItem) {
+      // console.log(menuItem.category);
+      if (menuItem.category === category) {
+        return menuItem;
+      }
+    });
+    if (category === "all") {
+      diplayMenuItems(menu);
+    } else {
+      diplayMenuItems(menuCategory);
+    }
+  });
+});
